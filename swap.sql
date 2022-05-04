3c177cac485a06889dc67c004b3f89116813b2e2
Aadhib <aadhibnasserv.hussain@gmail.com>
2022-05-04 18:51:15 +0530

    Rename swap.txt to swap.sql

---
+++ swap.sql
@@ -0,0 +1,21 @@
+﻿declare
+    a number;
+    b number;
+    temp number;
+ 
+begin
+    a:=5;
+    b:=10;
+    
+    dbms_output.put_line('before swapping:');
+    dbms_output.put_line('a='||a||' b='||b);
+    
+    temp:=a;
+    a:=b;
+    b:=temp;
+    
+    dbms_output.put_line('after swapping:');
+    dbms_output.put_line('a='||a||' b='||b);
+    
+end;
+/
